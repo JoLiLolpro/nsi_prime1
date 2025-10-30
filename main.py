2614d162919665cb9c0644deccb65b32e8c7b15f
JoLiLolpro <149590155+JoLiLolpro@users.noreply.github.com>
2025-10-30 19:39:42 +0100

    debut livre pencher

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ LCouleur = [
 ]
 
 legal_epeseur_values = [18, 36]
-legal_hauteur_values = [0.70, 1]
+legal_hauteur_values = [70, 100]
 
 class toto(Turtle):
     def gotos(self, x, y):
@@ -28,9 +28,13 @@ class toto(Turtle):
         luigi = []
         peach = 0
         while True:
-            toad = choice(legal_epeseur_values) # toad represente la taille de livre
-            mario = toad+4 # mario est plutot le livre ET la bordure
-            peach += mario 
+            if random() < 0.2: # livre pencher 20% de chance
+                toad = 0 # valeur qu'on pourra reconnaitre plus tard
+                mario = 36+4
+            else:
+                toad = choice(legal_epeseur_values) # toad represente la taille de livre
+                mario = toad+4 # mario est le livre ET la bordure
+            peach += mario
             if peach > 560-10: # ont calcule si ont ne depasse pas le cadre
                 return luigi
             luigi.append(toad)
@@ -54,7 +58,7 @@ class toto(Turtle):
         for i in range(2):
             self.fd(epeseur+4)
             self.left(90)
-            self.fd(100*hauteur+4)
+            self.fd(hauteur+4)
             self.left(90)
         self.end_fill()
         # va a l'interieur de cadre pour draw le livre
@@ -79,19 +83,30 @@ class toto(Turtle):
         for i in range(2):
             self.fd(epeseur)
             self.left(90)
-            self.fd(100*hauteur)
+            self.fd(hauteur)
             self.left(90)
         self.end_fill()
+
+    def livre_pencher(self):
+        self.livre("red", 36, 100)
+
+    def livres_avec_un_S(self, livres_liste):
+        """fait les livres sur l'etagere"""
+        for k in range(len(livres_liste)):
+            epeseur_temp = livres_liste[k]
+            if epeseur_temp == 0:
+                self.livre_pencher()
+                epeseur_temp = 36
+            else:
+                self.livre(choice(LCouleur), epeseur_temp, choice(legal_hauteur_values))
+            self.livre_transition(epeseur_temp)
 
     def etageres(self, nombre):
         """dessine une etagere composee de livres"""
         self.gotos(-300+10, -300+10) # ecart de 10px pour le premier livre
         for i in range(1, nombre+1):
             livres_liste = self.calcul_epeseur_livres()
-            for k in range(len(livres_liste)):
-                epeseur_temp = livres_liste[k]
-                self.livre(choice(LCouleur), epeseur_temp, choice(legal_hauteur_values))
-                self.livre_transition(epeseur_temp)
+            self.livres_avec_un_S(livres_liste)
             self.gotos(-300+10, -300+i*120)
 
 
